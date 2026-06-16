e09f723584e993a7b9f627db594973279354605c
Daniel Sakuma <dsakuma@gmail.com>
2026-06-16 10:20:56 -0300

    Convert to chezmoi

--- home/.wezterm.lua
+++
@@ -1,8 +0,0 @@
-local wezterm = require 'wezterm'
-
-local config = wezterm.config_builder()
-
-config.color_scheme = 'OneDark (base16)'
-config.font = wezterm.font 'MesloLGS Nerd Font Mono'
-
-return config
